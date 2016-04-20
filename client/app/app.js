e07469b91e9d57b68fccfcf07cfe3f4c91658857
Brian Best <brian.brianb64@gmail.com>
2016-04-20 15:02:49 -0300

    test

---
+++ client/app/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular.module('webappV2App', [
+  'webappV2App.constants',
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute',
+  'ui.bootstrap'
+])
+  .config(function($routeProvider, $locationProvider) {
+    $routeProvider
+      .otherwise({
+        redirectTo: '/'
+      });
+
+    $locationProvider.html5Mode(true);
+  });
