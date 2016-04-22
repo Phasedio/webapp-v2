6fdb2ebc4a9cc8ab92c891e794b205593edae6ee
Dave Riedstra <dave.riedstra@gmail.com>
2016-04-22 13:16:01 -0700

    unit tests for config, construction, and some auth things

---
+++ client/components/phased-provider/phased.provider.spec.js
@@ -0,0 +1,119 @@
+'use strict';
+
+describe('Component: PhasedProvider', function() {
+
+  // stub a module to get a ref to the provider
+  var PhasedProvider, sandbox;
+  // other modules to save
+  var $rootScope,
+    $http,
+    $location,
+    $window,
+    $firebaseAuth;
+
+  beforeEach(function (){
+    sandbox = sinon.sandbox.create();
+    // create the dummy module
+    angular.module('dummyModule', [])
+    .config(['PhasedProvider', function(_PhasedProvider) {
+      PhasedProvider = _PhasedProvider;
+    }]);
+
+    // inject into our module and stash some other modules
+    module('webappV2App', 'dummyModule');
+    inject(function(
+      _$rootScope_,
+      _$http_,
+      _$location_,
+      _$window_,
+      _$firebaseAuth_) {
+        $rootScope = _$rootScope_;
+        $http = _$http_;
+        $location = _$location_;
+        $window = _$window_;
+        $firebaseAuth = _$firebaseAuth_;
+
+        // spy on some modules
+        sandbox.spy(window, 'Firebase'); // constructor
+    });
+
+    // stub some methods
+    sandbox.stub(window.console, 'log');
+    sandbox.stub(window.console, 'warn');
+    sandbox.stub(window.console, 'error');
+  });
+
+  afterEach(function () {
+    sandbox.restore();
+  });
+
+  //
+  //  TESTS
+  //
+
+  // CONFIG & CONSTRUCTION
+  describe('construction and construction ($get)', function () {
+    it('should be an object', function () {
+      PhasedProvider.should.be.an('object');
+    });
+
+    // CONFIG
+    describe('#config', function () {
+      it('should return a promise', function () {
+        PhasedProvider.config().should.be.an.instanceOf(Promise);
+      });
+
+      it('should fail without a Firebase URL', function () {
+        PhasedProvider.config().should.be.rejected;
+      });
+    });
+
+    // CONSTRUCTION
+    describe('#construction', function () {
+      // create the Phased factory by injecting it (calls PhasedProvider.$get)
+      var Phased;
+      beforeEach(function () {
+        sandbox.spy(Firebase.prototype, 'onAuth');
+        inject(function (_Phased_){
+          Phased = _Phased_;
+        });
+      });
+
+      it('should provide Phased factory', function () {
+        expect(Phased).to.be.an('object');
+      });
+
+      it('should handle FB auth changes', function () {
+        assert(window.Firebase.called, 'FB was not instantiated');
+        assert(Firebase.prototype.onAuth.called, 'onAuth was not called');
+      });
+    });
+  });
+
+  // LOGIN AND LOGOUT
+  describe('login and logout', function () {
+    var Phased;
+    beforeEach(function () {
+      inject(function (_Phased_){
+        Phased = _Phased_;
+      });
+    });
+
+    it('should attempt to log in with FB', function () {
+      sandbox.spy(Firebase.prototype, 'authWithPassword');
+      Phased.login('d', 'a');
+      assert(Firebase.prototype.authWithPassword.called, 'did not call authWithPassword');
+    });
+
+    it('should gracefully fail with bad credentials', function () {
+      assert(false, 'gracefulness is not yet implemented');
+    })
+
+    // broken test; gives false negative
+    // it('should attempt to log out with FB', function () {
+    //   sandbox.spy(Firebase.prototype, 'unauth');
+    //   Phased.logout();
+    //   assert(Firebase.prototype.unauth.called, 'did not call unauth');
+    // });
+  })
+});
