c2d7c481e58a1d6bb4dc5f6a3f5f8c1abc4f57a6
Dave Riedstra <dave.riedstra@gmail.com>
2016-04-25 16:50:35 -0700

    very basic markup and controller stubs for assignments page

--- client/app/assignments/assignments.controller.js
+++ client/app/assignments/assignments.controller.js
@@ -10,6 +10,18 @@ var AssignmentsController = function AssignmentsController($http, $scope, Phased
   (function constructor() {
     $scope.Phased = Phased;
   })();
+
+  // when "Add New" is clicked, choose a new date?
+  $scope.onAddNew = function onAddNew(e) {
+    e.preventDefault();
+    console.log('onAddNew', e);
+  }
+
+  // when "Filter" is clicked, choose a new date?
+  $scope.onFilter = function onFilter(e) {
+    e.preventDefault();
+    console.log('onFilter', e);
+  }
 }
 
 angular.module('webappV2App')
