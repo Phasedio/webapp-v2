1a691afb9813ee50fb5e1c20a1fbbaf477391588
Dave Riedstra <dave.riedstra@gmail.com>
2016-04-25 14:45:31 -0700

    very basic markup and controls for reports page

--- client/app/reports/reports.controller.js
+++ client/app/reports/reports.controller.js
@@ -15,6 +15,36 @@ var ReportsController = function ReportsController($http, $scope, Phased) {
   $scope.postStatus = function postStatus() {
     Phased.postStatus($scope.statusName).then(() => $scope.statusName = '');
   }
+
+  // when "Today" is clicked, choose a new date?
+  $scope.onToday = function onToday(e) {
+    e.preventDefault();
+    console.log('onToday', e);
+  }
+ 
+  // when "Filter" is clicked
+  $scope.onFilter = function onFilter(e) {
+    e.preventDefault();
+    console.log('onFilter', e);
+  }
+
+  // when "Export" is clicked
+  $scope.onExport = function onExport(e) {
+    e.preventDefault();
+    console.log('onExport', e);
+  }
+
+  // when "Likes" is clicked
+  $scope.onLikes = function onLikes(e, status) {
+    e.preventDefault();
+    console.log('onLikes', e, status);
+  }
+
+  // when "Comments" is clicked
+  $scope.onComments = function onComments(e, status) {
+    e.preventDefault();
+    console.log('onComments', e, status);
+  }
 }
 
 angular.module('webappV2App')
