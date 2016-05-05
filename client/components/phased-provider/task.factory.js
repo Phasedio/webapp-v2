f959353c2369d673cda25922b18f4981ea89974d
Dave Riedstra <dave.riedstra@gmail.com>
2016-05-05 11:36:10 -0700

    accessors, comment manip, assignment manip

--- client/components/phased-provider/task.factory.js
+++ client/components/phased-provider/task.factory.js
@@ -79,6 +79,134 @@ angular.module('webappV2App')
 
 				// call super.destroy()
 			}
+
+			// 	ACCESSORS
+
+			/**		dueDate		*/
+			get dueDate() {
+				return this._.dueDate;
+			}
+
+			set dueDate(val) {
+				super.setProperty('dueDate', val);
+			}
+
+			/**		status		*/
+			get status() {
+				return this._.status;
+			}
+
+			set status(val) {
+				super.setProperty('status', val);
+			}
+
+			/**		assignment 	*/
+			get assignment() {
+				return this._.assignment;
+			}
+
+			set assignment(val) {
+				console.warn('Setting task assignment directly has no effect; please use Task#assignTo');
+				return false;
+			}
+
+			/**		comments 	*/
+			get comments() {
+				return this._.comments;
+			}
+
+			set comments(val) {
+				console.warn('Setting task comments directly has no effect; please use Task#addComment');
+				return false;
+			}
+
+			//	ASSIGNMENT MANIP
+
+			/**
+			*		Assigns a task to a member on your team
+			*	
+			*		@param	{string}	uid 	UID for a member on the team or null to un-assign
+			*		@throws TypeError				if uid is not a string
+			*		@throws	ReferenceError 					if uid is not a member of the team
+			*/
+			assignTo(uid) {
+				if (!(typeof uid == 'string' || uid == null)) {
+					throw new TypeError('uid should be String or null, got ' + (typeof uid));
+				}
+
+				if (!!uid && !(uid in Phased.team.members)) {
+					throw new ReferenceError(`Could not find member ${uid} in team`);
+				}
+
+				super.setProperty('assignment', {
+					to : uid,
+					by : Phased.user.uid,
+					time : Firebase.ServerValue.TIMESTAMP
+				});
+			}
+
+			//	COMMENT MANIP
+
+			/**
+			*		Adds a comment to the task
+			*
+			*		@param 	{string}	text 	text of the comment
+			*		@throws	TypeError				if text is not a string or empty
+			*/
+			addComment(text) {
+				if (!(typeof text == 'string') || text.length < 1) {
+					throw new TypeError('Cannot add empty comment to task');
+				}
+
+				return super.pushVal('comments', {
+					text : text,
+					user : Phased.user.uid,
+					time : Firebase.ServerValue.TIMESTAMP
+				});
+			}
+
+			/**
+			*		Deletes a comment locally and remotely
+			*
+			*		@param 	{string}	commentID		ID of the comment to delete
+			*		@throws	TypeError							if the comment ID is not a string
+			*		@throws	ReferenceError				if the comment is not found
+			*/
+			deleteComment(commentID) {
+				if (!(typeof commentID == 'string')) {
+					throw new TypeError('commentID should be string, got ' + (typeof commentID));
+				}
+
+				if (!(commentID in this.comments)) {
+					throw new ReferenceError(`Could not find comment ${commentID} for task ${this.ID}`);
+				}
+
+				super.setProperty(`comments/${commentID}`, null);
+			}
+
+			/**
+			*		Updates text for the comment at commentID
+			*
+			*		@param 	{string} 	commentID		ID of the comment to edit
+			*		@param 	{string}	text 				text of the comment
+			*		@throws	TypeError							if text is not a string or empty
+			*		@throws	ReferenceError				if the comment is not found
+			*/
+			editComment(commentID, text) {
+				if (!(typeof commentID == 'string')) {
+					throw new TypeError('commentID should be string, got ' + (typeof commentID));
+				}
+
+				if (!(typeof text == 'string')) {
+					throw new TypeError('commentID should be string, got ' + (typeof commentID));
+				}
+
+				if (!(commentID in this.comments)) {
+					throw new ReferenceError(`Could not find comment ${commentID} for task ${this.ID}`);
+				}
+
+				super.setProperty(`comments/${commentID}/text`, text);
+			}
 		}
 
 		/**	The task factory object */
