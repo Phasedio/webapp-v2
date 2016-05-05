9be8c6f3cee7280520556d4d5927d763b6b2a478
Dave Riedstra <dave.riedstra@gmail.com>
2016-05-05 15:45:55 -0700

    basic implementation and stubs

--- client/components/phased-provider/project.factory.js
+++ client/components/phased-provider/project.factory.js
@@ -35,57 +35,193 @@ angular.module('webappV2App')
 				// call super
 				super(FBRef.child(`/team/${Phased.team.uid}/projects/${ID}`));
 
-				// expand relevant properties from cfb
+				// expand relevant properties from cfg
 				({
 					name : this._.name,
-					taskID : this._.taskID,
 					dueDate : this._.dueDate,
 					assignment : this._.assignment,
 					comments : this._.comments,
-					status : this._.status
+					status : this._.status,
+					taskIDs : this._.taskIDs,
+					statusIDs : this._.statusIDs,
+					memberIDs : this._.memberIDs
 				} = cfg);
 
 				// register read-only properties
 				Object.defineProperty( this, 'created', {value: cfg.created, configurable:false, writeable:false, enumerable: true} );
 				
-				// link existing statuses
-				for (let id = Phased.statuses.length - 1; i >= 0; i--) {
-					if (!!Phased.statuses[id].projectID && Phased.statuses[id].projectID == ID) {
-						this.statuses[id] = Phased.statuses[id];
-					}
-				}
-
-				// link existing tasks
-				for (let id = Phased.tasks.length - 1; i >= 0; i--) {
-					if (!!Phased.tasks[id].projectID && Phased.tasks[id].projectID == ID) {
-						this.tasks[id] = Phased.tasks[id];
-					}
-				}
-
-				// update scope
-				$rootScope.$evalAsync(() => {
-					// link this to Phased.team.projects
-					Phased.team.projects[ID] = this;
-					
-					// broadcast PROJECT_ADDED
-					$rootScope.$broadcast(Phased.RUNTIME_EVENTS.PROJECT_ADDED);
-				});
+				// broadcast PROJECT_ADDED
+				$rootScope.$broadcast(Phased.RUNTIME_EVENTS.PROJECT_ADDED);
 			}
 
 			/*
 			*		Remove references that this project knows about
 			*		
-			*		@fires 	Phased#PROJECT_DELETED
+			*		@fires 	Phased#PROJECT_DESTROYED
 			*/
 			destroy() {
-				// delete reference in Phased.team.projects
-				delete Phased.team.projects[this.ID];
-
-				// fire PROJECT_DELETED
-				$rootScope.$broadcast(Phased.RUNTIME_EVENTS.PROJECT_DELETED);
+				// fire PROJECT_DESTROYED
+				$rootScope.$broadcast(Phased.RUNTIME_EVENTS.PROJECT_DESTROYED);
 
 				// call super.destroy()
 				super.destroy();
+			}
+
+			// 	ACCESSORS
+
+			/**		dueDate		*/
+			get dueDate() {
+				return this._.dueDate;
+			}
+
+			set dueDate(val) {
+				super.setProperty('dueDate', val);
+			}
+
+			/**		status		*/
+			get status() {
+				return this._.status;
+			}
+
+			set status(val) {
+				super.setProperty('status', val);
+			}
+
+			/**		comments 	*/
+			get comments() {
+				return this._.comments;
+			}
+
+			set comments(val) {
+				console.warn('Setting project comments directly has no effect; please use Project#addComment');
+				return false;
+			}
+
+			/**		memberIDs 	*/
+			get memberIDs() {
+				return this._.memberIDs;
+			}
+
+			set memberIDs(val) {
+				console.warn('Setting project memberIDs directly has no effect; please use Project#addMember');
+				return false;
+			}
+
+			/**		taskIDs 	*/
+			get taskIDs() {
+				return this._.taskIDs;
+			}
+
+			set taskIDs(val) {
+				console.warn('Setting project taskIDs directly has no effect; please use Project#addTask');
+				return false;
+			}
+
+			//	MEMBER MANIP
+
+			/**
+			*		Adds a team member to the project
+			*
+			*/
+			addMember(memberID) {
+
+			}
+
+			/**
+			*		Removes a team member from the project
+			*
+			*/
+			removeMember(memberID) {
+
+			}
+
+			// 	TASK MANIP
+
+			/**
+			*		Adds an existing task to the project
+			*
+			*/
+			addTask(taskID) {
+
+			}
+
+			/**
+			*		Creates a new task linked to the project
+			*
+			*/
+			createTask(cfg) {
+
+			}
+
+			/**
+			*		Removes a task from the project
+			*
+			*/
+			removeTask(taskID) {
+				
+			}
+
+			//	COMMENT MANIP
+
+			/**
+			*		Adds a comment to the task
+			*
+			*		@param 	{string}	text 	text of the comment
+			*		@throws	TypeError				if text is not a string or empty
+			*/
+			addComment(text) {
+				if (!(typeof text == 'string') || text.length < 1) {
+					throw new TypeError('Cannot add empty comment to task');
+				}
+
+				return super.pushVal('comments', {
+					text : text,
+					user : Phased.user.uid,
+					time : Firebase.ServerValue.TIMESTAMP
+				});
+			}
+
+			/**
+			*		Deletes a comment locally and remotely
+			*
+			*		@param 	{string}	commentID		ID of the comment to delete
+			*		@throws	TypeError							if the comment ID is not a string
+			*		@throws	ReferenceError				if the comment is not found
+			*/
+			deleteComment(commentID) {
+				if (!(typeof commentID == 'string')) {
+					throw new TypeError('commentID should be string, got ' + (typeof commentID));
+				}
+
+				if (!(commentID in this.comments)) {
+					throw new ReferenceError(`Could not find comment ${commentID} for task ${this.ID}`);
+				}
+
+				super.setProperty(`comments/${commentID}`, null);
+			}
+
+			/**
+			*		Updates text for the comment at commentID
+			*
+			*		@param 	{string} 	commentID		ID of the comment to edit
+			*		@param 	{string}	text 				text of the comment
+			*		@throws	TypeError							if text is not a string or empty
+			*		@throws	ReferenceError				if the comment is not found
+			*/
+			editComment(commentID, text) {
+				if (!(typeof commentID == 'string')) {
+					throw new TypeError('commentID should be string, got ' + (typeof commentID));
+				}
+
+				if (!(typeof text == 'string')) {
+					throw new TypeError('commentID should be string, got ' + (typeof commentID));
+				}
+
+				if (!(commentID in this.comments)) {
+					throw new ReferenceError(`Could not find comment ${commentID} for task ${this.ID}`);
+				}
+
+				super.setProperty(`comments/${commentID}/text`, text);
 			}
 		}
 
@@ -114,10 +250,14 @@ angular.module('webappV2App')
 				let cfg = snap.val();
 				let id = snap.key();
 
-				new Project(id, cfg);
+				$rootScope.$evalAsync( () => Phased.team.projects[id] = new Project(id, cfg) );
 			});
 		});
 
+		// manage deleted project references
+		$rootScope.$on(Phased.RUNTIME_EVENTS.PROJECT_DESTROYED, (projectID) => {
+			delete Phased.team.projects[projectID];
+		});
 
 		return ProjectFactory;
 	}]);
