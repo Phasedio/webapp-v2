949319270ef85e1e4a10e9f50d653637d1cee681
Brian Best <brian.brianb64@gmail.com>
2016-05-10 15:38:24 -0300

    all components for projects are on screen.

--- client/app/projects/projects.controller.js
+++ client/app/projects/projects.controller.js
@@ -1,16 +1,25 @@
 'use strict';
 (function(){
 
-class ProjectsComponent {
-  constructor() {
-    this.message = 'Hello';
-  }
-}
+var ProjectsController = function ProjectsController ($http, $scope, Phased, StatusFactory, TaskFactory, ProjectFactory){
+  (function constructor() {
+    $scope.Phased = Phased;
+    
+  })();
+  $scope.hasTasks = function hasTasks(obj){
+    if(_.size(obj) > 0){
+      return true;
+    }else{
+      return false;
+    }
+  };
+};
+
 
 angular.module('webappV2App')
   .component('projects', {
     templateUrl: 'app/projects/projects.html',
-    controller: ProjectsComponent
+    controller: ProjectsController
   });
 
 })();
