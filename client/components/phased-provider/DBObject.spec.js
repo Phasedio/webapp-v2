ed12e560d92d9b0b501247f79e846ee73c4aaa71
Dave Riedstra <dave.riedstra@gmail.com>
2016-05-11 09:51:41 -0700

    better stubbing of Firebase and DBObject

--- client/components/phased-provider/DBObject.spec.js
+++ client/components/phased-provider/DBObject.spec.js
@@ -14,36 +14,40 @@ describe('Class: DBObject', function() {
   var lastPushed;
   // data fed to update gets stashed here
   var lastUpdated;
+  // a stub Firebase
+  var Firebase = function Firebase(FURL) {}
 
   beforeEach(function() {
-    sandbox.spy(window, 'Firebase'); // constructor
-
-    // stub some firebase methods to return some dummy data
-    sandbox.spy(Firebase.prototype, 'onAuth');
-    sandbox.spy(Firebase.prototype, 'unauth');
-    sandbox.stub(Firebase.prototype, 'child', function() {
-      return FBRefStub;
+    Firebase.prototype.onAuth = sandbox.spy();
+    Firebase.prototype.unauth = sandbox.spy();
+    Firebase.prototype.child = sandbox.spy(function() {
+      return new FBRefStub();
     });
+    sandbox.stub(window, 'Firebase', Firebase); // replace window Firebase object with our own stubbed version
 
     snapStub = sandbox.stub().returnsPromise().resolves({})();
     snapStub.key = sandbox.stub();
     snapStub.val = sandbox.stub();
 
-    FBRefStub = {
-      push: function(data) {
-        lastPushed = data;
-        return snapStub;
-      },
-      update: function(data) {
-        lastUpdated = data;
-        return snapStub;
-      },
-      set: sandbox.stub(),
-      on: sandbox.stub().returnsPromise().resolves({}),
-      once: sandbox.stub().returnsPromise().resolves({})
+    class FBRefStubCl extends Firebase {
     };
-    sandbox.spy(FBRefStub, 'push');
-    sandbox.spy(FBRefStub, 'update');
+
+    FBRefStubCl.prototype.then = sandbox.stub();
+    FBRefStubCl.prototype.key = sandbox.stub();
+    FBRefStubCl.prototype.val = sandbox.stub();
+    FBRefStubCl.prototype.set = sandbox.stub();
+    FBRefStubCl.prototype.on = sandbox.stub().returnsPromise().resolves({});
+    FBRefStubCl.prototype.once = sandbox.stub().returnsPromise().resolves({});
+    FBRefStubCl.prototype.push = sandbox.spy(function (data) {
+      lastPushed = data;
+      return snapStub;
+    });
+    FBRefStubCl.prototype.update = sandbox.spy(function (data) {
+      lastUpdated = data;
+      return snapStub;
+    });
+
+    FBRefStub = FBRefStubCl;
 
     // create the dummy module
     angular.module('dummyModule', [])
@@ -67,11 +71,33 @@ describe('Class: DBObject', function() {
   //
 
   describe('#constructor', function () {
+    // create an object to house "methods" to spy on
+    var that = {};
+
+    beforeEach(function () {
+      that.DBObject = DBObject;
+      sandbox.spy(that, 'DBObject'); // spy on DBObject constructor
+      // a class child to extend DBObject 
+      class DBObjectChild extends that.DBObject {
+        constructor() {
+          var myFb = new Firebase();
+          super(myFb.child('never!'));
+        }
+      }
+      that.DBObjectChild = DBObjectChild;
+    });
+
+
   	it('should fail when called directly (needs to be extended)', function () {
   		var mkDBO = function mkDBO() {
   			new DBObject();
   		}
   		expect(mkDBO).to.throw(Error);
   	})
+
+    it('should be called when a child class is instantiated', function () {
+      new that.DBObjectChild();
+      assert(that.DBObject.called, 'DBObject constructor was not called');
+    })
   })
 });
