269b0f88ff65f18f7c1417a83863589488de2137
Dave Riedstra <dave.riedstra@gmail.com>
2016-05-13 11:57:32 -0700

    add stripe and mandrill secrets to config vars

--- server/config/environment/index.js
+++ server/config/environment/index.js
@@ -43,6 +43,8 @@ var all = {
 
   FSEC : 'A50wFi5OxaLYNzb4jnEyFMQWmE8mjRyWJCKW723g',  // DB SECRECT
   FUID: 'phased-server'                               // AUTH UID
+  STRIPE_SECRET : 'sk_live_nKZ1ouWkI3WuiVGK2hIvZUH1', // Stripe secret -- we should use the test one for dev
+  MANDRILL_SECRET : 'B0N7XKd4RDy6Q7nWP2eFAA'          // Mandrill secret
 };
 
 // Export the config object based on the NODE_ENV
